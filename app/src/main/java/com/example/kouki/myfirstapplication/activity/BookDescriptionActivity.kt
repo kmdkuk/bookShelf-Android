391d0dab9e82528775defa4d50cba02577596a8d
kouki <kouworld0123@gmail.com>
2018-12-31 23:10:06 +0900

    :sparkles: descriptionで全情報をとりあえず表示

--- app/src/main/java/com/example/kouki/myfirstapplication/activity/BookDescriptionActivity.kt
+++ app/src/main/java/com/example/kouki/myfirstapplication/activity/BookDescriptionActivity.kt
@@ -12,10 +12,16 @@ class BookDescriptionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_description)
 
-        val intent = intent
         // MainActivityからintentで受け取ったものを取り出す
         val selectedBook = intent.getSerializableExtra("Book") as BookModel
         titleTextView.text = selectedBook.title
         authorsTextView.text = selectedBook.authors
+        descriptionTextView.text = selectedBook.description
+        publishedDateTextView.text = selectedBook.publishDate
+        categoriesTextView.text = selectedBook.categories
+        boughtDateTextView.text = selectedBook.boughtDate
+        readDateTextView.text = selectedBook.readDate
+        progressTextView.text = selectedBook.progress
+        notesTextView.text = selectedBook.notes
     }
 }
