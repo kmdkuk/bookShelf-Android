f4f0fb6cce16e8d6fe968749a0fea38791c73033
kouki <kouworld0123@gmail.com>
2018-12-31 21:31:50 +0900

    :+1: BookContractを修正

--- app/src/main/java/com/example/kouki/myfirstapplication/database/BookContract.kt
+++ app/src/main/java/com/example/kouki/myfirstapplication/database/BookContract.kt
@@ -8,7 +8,13 @@ class BookContract : BaseColumns {
 
         const val _ID = "_id"
         const val COLUMN_NAME_BOOK_TITLE = "title"
-        const val COLUMN_NAME_BOOK_AUTHOR = "author"
-        const val COLUMN_NAME_BOOK_PRICE = "price"
+        const val COLUMN_NAME_BOOK_AUTHORS = "author"
+        const val COLUMN_NAME_BOOK_DESCRIPTION = "description"
+        const val COLUMN_NAME_BOOK_PUBLISHED_DATE = "published_date"
+        const val COLUMN_NAME_BOOK_CATEGORIES = "categories"
+        const val COLUMN_NAME_BOOK_BOUGHT_DATE = "bought_date"
+        const val COLUMN_NAME_BOOK_READ_DATE = "read_date"
+        const val COLUMN_NAME_BOOK_PROGRESS = "progress"
+        const val COLUMN_NAME_BOOK_NOTES = "notes"
     }
 }
